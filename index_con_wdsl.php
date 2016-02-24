c681c6d4a3f733a1a8984e10f433d49636869c94
jjaviergr1176 <jjaviergr1176@gmail.com>
2016-02-24 12:34:38 +0100

    servicio GPS montando.

--- index_con_wdsl.php
+++ index_con_wdsl.php
@@ -4,37 +4,30 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
-    <body>
-        <h3>Bienvenido a Administra coordenadas</h3>
-        <select multiple>
-            <option>Insertar Coordenada</option>
-            <option>Mostrar Coordenadas entre un intervalo de fechas</option>
-            <option selected>Mostrar todas las Coordenadas de la BD</option>
-            <option>Mostrar intervalos de fecha grabados en la BD</option>
-            <option>Borrar intervalo de coordenadas seleccionando por tiempo</option>
-            <option>Borrar todas las coordenadas</option>
-        </select>
-        <?php
-        require_once ('gps.php');
 
-        $uri = "http://localhost/receta_soap_wdsl";
+    <?php
+    require_once ('gps.php');
+
+    $uri = "http://localhost/receta_soap_wdsl";
 
 
-
-        try {
-
-        
+    //print lanzador();
+    try {
         $cliente = new SoapClient("$uri/BD_Proxy.wsdl");
-        print "Tipos :";
-        print_r($cliente->__getTypes());
-        print "<br><br>";
-        print "Funciones :";
-        print_r($cliente->__getFunctions());
-        print "<br><br>";
-        //print_r($cliente->__getFunctions());
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+        print($cliente->__getLastResponse());
+    }
 
+
+
+//        print "\n Tipos :";
+//        print_r($cliente->__getTypes());
+//        print "<br><br>";
+//        print "\n Funciones :";
+//        print_r($cliente->__getFunctions());
+//        print "<br><br>";
 //        $cliente->__soapCall('obtieneNombreReceta', array('codigo' => 1));
-
 //       $receta= $cliente->obtieneNombreReceta(1); 
 //       echo ("receta es : " + $receta); // funciona correctamente.
 //     $cliente->__soapCall("obtieneNombreReceta",array(1));
@@ -51,11 +44,6 @@
 //        print_r($cliente->obtener_array_ingredientes_receta(1));
 //        print "<br><br>";
 //        
-        }
-        catch (Exception $ex) {
-              echo $ex->getMessage();
-              print($cliente->__getLastResponse());
-        }
 //        
 //        try
 //        {
@@ -84,6 +72,25 @@
 //              echo $ex->getMessage();
 //              print($cliente->__getLastResponse());
 //        }
-        ?>
+    ?>
+    <script>
+        function lanzador()
+        {
+            var cad=" print(<p>AAAAAAAA</p>);"
+        
+            document.write("<?php print(<p>AAAAAAAA</p>) ?>");
+        }
+    </script>
+    <body>
+        <h3>Bienvenido a Administra coordenadas</h3>
+        <select multiple onchange='lanzador();">
+            <option id="1">Insertar Coordenada</option>
+            <option id="2">Mostrar Coordenadas entre un intervalo de fechas</option>
+            <option id="3" selected>Mostrar todas las Coordenadas de la BD</option>
+            <option id="4">Mostrar intervalos de fecha grabados en la BD</option>
+            <option id="5">Borrar intervalo de coordenadas seleccionando por tiempo</option>
+            <option id="6">Borrar todas las coordenadas</option>
+        </select>
+
     </body>
 </html>
